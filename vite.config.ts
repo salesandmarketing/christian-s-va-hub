40279de7e17818a9f8db0a5c4fbfda0d97553af6
githubbest729 <153446032+githubbest729@users.noreply.github.com>
2026-02-09 17:19:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc' // Add '-swc' here
 import path from 'path'
 
 export default defineConfig(({ mode }) => ({
